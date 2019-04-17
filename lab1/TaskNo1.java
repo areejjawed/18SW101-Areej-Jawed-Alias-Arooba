6df9be3e7b2386887438cace68f05b07c536ccbc
areejjawed <49605420+areejjawed@users.noreply.github.com>
2019-04-17 12:26:45 +0500

    Add files via upload

---
+++ lab1/TaskNo1.java
@@ -0,0 +1,15 @@
+public class TaskNo1{
+public static void main(String args[]){
+String name;
+name="Areej";
+System.out.println("Name:"+name);
+short totalmarks=1100;
+System.out.println("Total marks:"+totalmarks);
+int obtainedmarks=987;
+System.out.println("Obtained marks:"+obtainedmarks);
+char grade='A';
+System.out.println("Grade:"+grade);
+double per=(obtainedmarks*100)/totalmarks;
+System.out.println("Percentage:"+per);
+}
+}
---
+++ lab1/TaskNo2.java
@@ -0,0 +1,11 @@
+class TaskNo2{
+
+public static void main(String args[])
+{
+double a=56.50;
+int b=(int)56.50;
+//float c=a-b;
+System.out.println(a);
+System.out.println(b);
+}
+}
