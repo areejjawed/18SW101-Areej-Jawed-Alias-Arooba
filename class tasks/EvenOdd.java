bb17f3ef999b63bc56ac71eaeca0f38af87d7e2d
areejjawed <49605420+areejjawed@users.noreply.github.com>
2019-05-11 06:33:07 -0700

    Add files via upload

---
+++ class tasks/EvenOdd.java
@@ -0,0 +1,21 @@
+class EvenOdd{
+public static void main(String args[]){
+System.out.println("Even numbers:");
+for(int i=0;i<=25;i++)
+{
+   if (i%2 !=0)
+{
+continue;
+}
+  System.out.println("Odd numbers:");
+for(int j=0;j<=25;j++)
+{
+   if (j%2 ==0)
+{
+continue;
+}
+System.out.println(i);
+    }
+  }
+}
+}
